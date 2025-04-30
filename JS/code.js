b7019dc576503627e1b9f23744a48a5408f51b01
TATNOOOOOO <167925266+TATNOOOOOO@users.noreply.github.com>
2025-04-30 20:33:50 +0900

    Update code.js

--- JS/code.js
+++ JS/code.js
@@ -8,16 +8,25 @@ $(function(){
     }
   });
   
-
   let targetWidth = 1920;
 
   function applyScale() {
     let screenWidth = window.innerWidth;
     let scaleFactor = screenWidth / targetWidth;
   
-    document.body.style.transform = `scale(${scaleFactor})`;
-    document.body.style.transformOrigin = 'top left';
+    // PCより小さいときだけ縮小
+    if (scaleFactor < 1) {
+      document.body.style.transform = `scale(${scaleFactor})`;
+      document.body.style.transformOrigin = 'top left';
+      document.body.style.width = `${targetWidth}px`; // レイアウト崩れ防止
+    } else {
+      // PCは等倍表示（拡大しない）
+      document.body.style.transform = '';
+      document.body.style.transformOrigin = '';
+      document.body.style.width = '';
+    }
   }
   
   window.addEventListener('load', applyScale);
   window.addEventListener('resize', applyScale);
+  
